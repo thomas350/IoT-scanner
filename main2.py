022fd8208c95731bf1d526dd87b2acadc5c409f9
bamboular <128512205+bamboular@users.noreply.github.com>
2023-03-27 15:21:23 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -119,7 +119,7 @@ def exploit_db_script():
     print("-----Welcome to exploitDB script check-----\n")
     url = "https://www.exploit-db.com/search"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
     search_term = input('recherche : ')
     params = {'action': 'search', 'value': search_term}
